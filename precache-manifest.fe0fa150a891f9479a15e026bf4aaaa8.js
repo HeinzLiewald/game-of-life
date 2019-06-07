7e417d25d4faa5563fe1363e97a831ed4afe4cf7
Heinz Liewald <heinz.liewald@onmobile.com>
2019-06-07 15:02:39 +0200

    Updates

---
+++ precache-manifest.fe0fa150a891f9479a15e026bf4aaaa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da5f8def056b10769453266da72e257",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "80f83c340d6d90995cad",
+    "url": "/game-of-life/static/css/main.53da8ea2.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/game-of-life/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "80f83c340d6d90995cad",
+    "url": "/game-of-life/static/js/main.00f6d70a.chunk.js"
+  },
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  }
+]);
